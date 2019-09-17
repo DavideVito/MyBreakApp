1ae2d2269840f65354bfabc84de94a089a2b5095
davidevitiello <davidevitiello@192.168.1.104>
2019-09-17 20:05:51 +0200

    Miglioramenti generali

--- MyBreakApp/API/CSS/ottieniStilleCss.php
+++
@@ -1,3 +0,0 @@
-<?php
-
-echo '        <link rel="stylesheet" href="https://' . $_SERVER['HTTP_HOST'] . '/MyBreakApp/API/CSS/stile.css?v=' . time() .'/>';
--- MyBreakApp/API/JS/OttieniMain.php
+++
@@ -1,3 +0,0 @@
-<?php
-    echo '<script src="https://' . $_SERVER['HTTP_HOST'] . '/MyBreakApp/API/JS/app.js?v='. time()  .'"></script>';
-?>
